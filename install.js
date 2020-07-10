2f7b1ca6133b050fc755753198c84e1ce2e9d1ae
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-07-09 20:38:42 -0700

    Link Update
    Hopefully allows the plugin to be useable...

--- install.js
+++ install.js
@@ -1,8 +1,8 @@
 var script = document.createElement("script");
-script.src = "script.js";
+script.src = "/script.js";
 script.defer = true;
 document.head.appendChild(script);
 var styles = document.createElement("link");
-styles.href = "styles.css";
+styles.href = "/styles.css";
 styles.rel = "stylesheet";
 document.head.appendChild(styles);
