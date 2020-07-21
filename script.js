a28e094538b6e70bed71c854b8f4aa78dc95f62f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-07-20 20:47:55 -0700

    Calculations Re-Rewrite
    The last update appeared to actually make the issue worse than it was before the rewrite, so I went back to the original implementation with a few fixes that should help with a few more issues that could occur. Typing performance in textareas with 1000+ lines of text is still a sub-par experience for what I hope to achieve, so I hope I will be able to find a better performing method than how it currently functions.

--- script.js
+++ script.js
@@ -11,36 +11,47 @@ numberedContainers.forEach(function(container){
     updateLineCount(textarea);
   });
   textarea.addEventListener("scroll",function(){
-    var scrollbarHeight = textarea.offsetHeight - textarea.clientHeight;
-    if (scrollbarHeight > 0){
-      lineCount.style.paddingBottom = `${parseInt(window.getComputedStyle(lineCount).getPropertyValue("padding-top")) + scrollbarHeight}px`;
-    } else {
-      lineCount.style.removeProperty("padding-bottom");
-    }
-    lineCount.scrollTop = textarea.scrollTop;
+    updateScrollPosition(textarea);
   });
 });
 function updateLineCount(textarea){
-  var lineCount = textarea.parentElement.getElementsByTagName("ol")[0];
-  var previousCount = textarea.dataset.numberedRows;
-  if (previousCount == undefined){
-    previousCount = 0;
-  }
-  textarea.style.padding = 0;
-  textarea.style.height = 0;
-  var currentCount = Math.floor(parseInt(textarea.scrollHeight) / parseInt(window.getComputedStyle(textarea).getPropertyValue("line-height")));
-  textarea.style.removeProperty("padding");
-  textarea.style.removeProperty("height");
-  var countDifference = currentCount - previousCount;
-  if (countDifference != 0){
-    for (i = 0; i < Math.abs(countDifference); i++){
-      if (countDifference > 0){
-        lineCount.appendChild(document.createElement("li"));
-      }
-      if (countDifference < 0){
-        lineCount.lastChild.remove();
+  if (textarea.parentElement.hasAttribute("data-numbered-hidden") == false){console.log("Updated!");
+    var lineCount = textarea.parentElement.getElementsByTagName("ol")[0];
+    var previousCount = textarea.getAttribute("data-numbered-rows");
+    if (previousCount == undefined){
+      previousCount = 0;
+    }
+    var currentCount = textarea.value.split("\n").length;
+    var countDifference = currentCount - previousCount;
+    if (countDifference != 0){
+      for (i = 0; i < Math.abs(countDifference); i++){
+        if (countDifference > 0){
+          lineCount.appendChild(document.createElement("li"));
+        }
+        if (countDifference < 0){
+          lineCount.lastChild.remove();
+        }
       }
     }
+    textarea.setAttribute("data-numbered-rows",currentCount);
+    updateScrollPosition(textarea);
   }
-  textarea.dataset.numberedRows = currentCount;
+}
+function updateScrollPosition(textarea){
+  var lineCount = textarea.parentElement.getElementsByTagName("ol")[0];
+  var scrollbarHeight = textarea.offsetHeight - textarea.clientHeight;
+  if (scrollbarHeight > 0){
+    lineCount.style.paddingBottom = `${parseInt(window.getComputedStyle(lineCount).getPropertyValue("padding-top")) + scrollbarHeight}px`;
+  } else {
+    lineCount.style.removeProperty("padding-bottom");
+  }
+  lineCount.scrollTop = textarea.scrollTop;
+}
+function toggleLineCount(textarea){
+  if (textarea.parentElement.hasAttribute("data-numbered-hidden")){
+    textarea.parentElement.removeAttribute("data-numbered-hidden");
+    updateLineCount(textarea);
+  } else {
+    textarea.parentElement.setAttribute("data-numbered-hidden",true);
+  }
 }
