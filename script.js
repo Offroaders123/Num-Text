c62158313bb2aa5a86246699f34a15cf7facf262
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2020-11-30 01:51:52 -0800

    Element Prototype Functions + Script Loading Changes + Browser Appearance Fixes
    Changes:
    - Numbered containers are no longer established automatically when loading the plugin's script, this now has to be done manually by the implementor.
    - All plugin functions have been converted into custom prototypes that extend the Element object.
    - Added a new function to manage the value of a textarea, as well as automatically refresh its line count after changing the value.
    - Textareas now have a minimum width and height to prevent the element from becoming too small when resizing it with the resize handle visible on some browsers.
    
    Fixes:
    - Textareas now use the "wrap" attribute instead of the "white-space: nowrap" CSS property. This change was made because "white-space: nowrap" didn't work consistently on Safari after changing the textarea's value programatically.
    - Added a few styles to counteract unintended appearances from default values set by various user agents.

--- script.js
+++ script.js
@@ -1,76 +1,91 @@
-var numberedContainers = Array.from(document.querySelectorAll("[data-numbered-container]"));
-numberedContainers.forEach(function(container){
-  establishNumberedContainer(container);
-});
-function establishNumberedContainer(container){
-  var lineCount = document.createElement("ol"), textarea = container.getElementsByTagName("textarea")[0];
+Element.prototype.num_establishContainer = function(){
+  var container = this, lineCount = document.createElement("ol"), textarea = container.querySelector("textarea");
   container.insertBefore(lineCount,textarea);
-  updateLineCount(textarea);
-  lineCount.addEventListener("click",function(){
-    textarea.focus();
-  });
+  textarea.num_refreshLineCount();
+  lineCount.addEventListener("mousedown",focusTextarea);
+  lineCount.addEventListener("click",focusTextarea);
+  textarea.wrap = "off";
   textarea.addEventListener("keydown",function(event){
-    if ((event.ctrlKey || event.metaKey) && (event.key == "z" || event.key == "Z" || event.key == "y")){
-      textarea.setAttribute("data-numbered-update",true);
-    }
+    if ((event.ctrlKey || event.metaKey) && (event.key == "z" || event.key == "Z" || event.key == "y")) textarea.setAttribute("data-numbered-refresh",true);
     if (event.key != "Enter" && event.key != "Backspace" && event.key != "Delete") return;
     var removedText = "", start = textarea.selectionStart, end = textarea.selectionEnd, singleCharacter = (start == end);
-    if (!singleCharacter){
-      removedText = textarea.value.substring(start,end);
-    } else {
+    if (singleCharacter){
       if (event.key == "Backspace" && start != 0) removedText = textarea.value.substring(start - 1,start);
       if (event.key == "Delete" && start != textarea.value.length) removedText = textarea.value.substring(start,start + 1);
-    }
-    if (event.key == "Enter" || removedText.includes("\n")) textarea.setAttribute("data-numbered-update",true);
+    } else removedText = textarea.value.substring(start,end);
+    if (event.key == "Enter" || removedText.includes("\n")) textarea.setAttribute("data-numbered-refresh",true);
   });
   textarea.addEventListener("cut",function(){
-    textarea.setAttribute("data-numbered-update",true);
+    textarea.setAttribute("data-numbered-refresh",true);
   });
   textarea.addEventListener("paste",function(){
-    textarea.setAttribute("data-numbered-update",true);
+    textarea.setAttribute("data-numbered-refresh",true);
   });
   textarea.addEventListener("input",function(){
-    if (!textarea.hasAttribute("data-numbered-update")) return;
-    updateLineCount(textarea);
-    textarea.removeAttribute("data-numbered-update");
+    if (!textarea.hasAttribute("data-numbered-refresh")) return;
+    textarea.num_refreshLineCount();
+    textarea.removeAttribute("data-numbered-refresh");
   });
   textarea.addEventListener("scroll",function(){
-    updateScrollPosition(textarea);
+    textarea.num_refreshScrollPosition();
   },{ passive: true });
-}
-function updateLineCount(textarea){
-  if (textarea.parentElement.hasAttribute("data-numbered-hidden")) return;
-  var lineCount = textarea.parentElement.getElementsByTagName("ol")[0], previousCount = textarea.getAttribute("data-numbered-rows");
-  if (!previousCount) previousCount = 0;
-  var currentCount = textarea.value.split("\n").length, countDifference = currentCount - previousCount;
-  if (countDifference == 0) return;
-  for (i = 0; i < Math.abs(countDifference); i++){
-    if (countDifference > 0) lineCount.appendChild(document.createElement("li"));
-    if (countDifference < 0) lineCount.lastChild.remove();
+  new ResizeObserver(function(){
+    if (textarea.parentElement != container) return console.error(`${textarea} is not within it's associated numbered container parent`,textarea,container);
+    container.style.removeProperty("width");
+    container.style.height = `${container.offsetHeight - container.clientHeight + parseInt(textarea.style.height.replace(/px/g,""))}px`;
+    textarea.style.removeProperty("height");
+  }).observe(textarea);
+  function focusTextarea(){
+    event.preventDefault();
+    if (document.activeElement != textarea) textarea.focus();
   }
-  textarea.setAttribute("data-numbered-rows",currentCount);
-  updateScrollPosition(textarea);
 }
-function updateScrollPosition(textarea){
-  var lineCount = textarea.parentElement.getElementsByTagName("ol")[0], scrollbarHeight = textarea.offsetHeight - textarea.clientHeight, paddingHeight = parseInt(window.getComputedStyle(lineCount).getPropertyValue("padding-top")), overflowOffset = parseInt(window.getComputedStyle(lineCount).getPropertyValue("--overflow-offset"));
+Element.prototype.num_refreshLineCount = function(){
+  var textarea = this;
+  if (textarea.parentElement.hasAttribute("data-numbered-hidden")) return;
+  var lineCount = textarea.parentElement.querySelector("ol"), previousCount = textarea.getAttribute("data-numbered-lines");
+  if (!previousCount) previousCount = 0;
+  var currentCount = ((textarea.value.match(/\n/g)||[]).length + 1), countDifference = currentCount - previousCount;
+  if (countDifference == 0) return;
+  if (countDifference > 0){
+    if (countDifference > 50){
+      var fragment = new DocumentFragment();
+      for (i = 0; i < countDifference; i++) fragment.appendChild(document.createElement("li"));
+      lineCount.appendChild(fragment);
+    } else {
+      for (i = 0; i < countDifference; i++) lineCount.appendChild(document.createElement("li"));
+    }
+  }
+  if (countDifference < 0) for (i = 0; i < Math.abs(countDifference); i++) lineCount.lastChild.remove();
+  textarea.setAttribute("data-numbered-lines",currentCount);
+  textarea.num_refreshScrollPosition();
+}
+Element.prototype.num_refreshScrollPosition = function(){
+  var textarea = this, lineCount = textarea.parentElement.querySelector("ol"), scrollbarHeight = textarea.offsetHeight - textarea.clientHeight, paddingHeight = parseInt(window.getComputedStyle(lineCount).getPropertyValue("padding-top")), overflowOffset = parseInt(window.getComputedStyle(lineCount).getPropertyValue("--overflow-offset"));
   if (scrollbarHeight > 0){
     lineCount.style.setProperty("--overflow-offset",`${paddingHeight + scrollbarHeight}px`);
-  } else {
-    lineCount.style.setProperty("--overflow-offset",`${paddingHeight}px`);
-  }
+  } else lineCount.style.setProperty("--overflow-offset",`${paddingHeight}px`);
   lineCount.scrollTop = textarea.scrollTop;
 }
-function enableLineCount(textarea){
+Element.prototype.num_enableLineCount = function(){
+  var textarea = this;
   textarea.parentElement.removeAttribute("data-numbered-hidden");
-  updateLineCount(textarea);
+  textarea.num_refreshLineCount();
 }
-function disableLineCount(textarea){
+Element.prototype.num_disableLineCount = function(){
+  var textarea = this;
   textarea.parentElement.setAttribute("data-numbered-hidden",true);
 }
-function toggleLineCount(textarea){
+Element.prototype.num_toggleLineCount = function(){
+  var textarea = this;
   if (textarea.parentElement.hasAttribute("data-numbered-hidden")){
-    enableLineCount(textarea);
-  } else {
-    disableLineCount(textarea);
-  }
+    textarea.num_enableLineCount();
+  } else textarea.num_disableLineCount();
+}
+Element.prototype.num_changeValue = function(value){
+  var textarea = this;
+  if (textarea.tagName.toLowerCase() != "textarea") return console.error(`${textarea} is not a valid numbered textarea`,textarea);
+  if (!textarea.parentElement.hasAttribute("data-numbered-container")) return console.error(`${textarea} is not within a valid numbered container parent`,textarea);
+  textarea.value = value;
+  textarea.num_refreshLineCount();
 }
