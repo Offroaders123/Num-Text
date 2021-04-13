429ace6879f5610dc44749d5037a0593bba9bd74
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2021-04-13 11:30:40 -0700

    Web Component Rewrite
    I reworked the plugin from the ground up to use the Web Components API! Instead of creating a numbered textarea with <div> and <textarea> elements, you only have to add a <num-text> element, and all of the heavy lifting from the plugin will be hidden away in it's Shadow DOM. Also, you only have to include the plugin's script tag in your document now, not the stylesheet. The plugin will add the styles to the <num-text> element's Shadow DOM directly using a <link> tag.

--- script.js
+++ script.js
@@ -1,85 +1,107 @@
-Element.prototype.num_establishContainer = function(){
-  var container = this, lineCount = document.createElement("ol"), textarea = container.querySelector("textarea");
-  container.insertBefore(lineCount,textarea);
-  textarea.num_refreshLineCount();
-  lineCount.addEventListener("mousedown",focusTextarea);
-  lineCount.addEventListener("click",focusTextarea);
-  textarea.wrap = "off";
-  textarea.addEventListener("keydown",event => {
-    if ((event.ctrlKey || event.metaKey) && (event.key == "z" || event.key == "Z" || event.key == "y")) textarea.setAttribute("data-numbered-refresh",true);
-    if (event.key != "Enter" && event.key != "Backspace" && event.key != "Delete") return;
-    var removedText = "", start = textarea.selectionStart, end = textarea.selectionEnd, singleCharacter = (start == end);
-    if (singleCharacter){
-      if (event.key == "Backspace" && start != 0) removedText = textarea.value.substring(start - 1,start);
-      if (event.key == "Delete" && start != textarea.value.length) removedText = textarea.value.substring(start,start + 1);
-    } else removedText = textarea.value.substring(start,end);
-    if (event.key == "Enter" || removedText.includes("\n")) textarea.setAttribute("data-numbered-refresh",true);
-  });
-  textarea.addEventListener("cut",() => textarea.setAttribute("data-numbered-refresh",true));
-  textarea.addEventListener("paste",() => textarea.setAttribute("data-numbered-refresh",true));
-  textarea.addEventListener("input",() => {
-    if (!textarea.hasAttribute("data-numbered-refresh")) return;
-    textarea.num_refreshLineCount();
-    textarea.removeAttribute("data-numbered-refresh");
-  });
-  textarea.addEventListener("scroll",() => textarea.num_refreshScrollPosition(),{ passive: true });
-  new ResizeObserver(() => {
-    if (textarea.parentElement != container) return console.error(`${textarea} is not within it's associated numbered container parent`,textarea,container);
-    container.style.removeProperty("width");
-    container.style.height = `${container.offsetHeight - container.clientHeight + parseInt(textarea.style.height.replace(/px/g,""))}px`;
-    textarea.style.removeProperty("height");
-  }).observe(textarea);
-  function focusTextarea(){
-    event.preventDefault();
-    if (document.activeElement != textarea) textarea.focus();
+class NumText extends HTMLElement {
+  constructor(){
+    super();
+    this.attachShadow({ mode: "open" });
   }
-}
-Element.prototype.num_refreshLineCount = function(){
-  var textarea = this;
-  if (textarea.parentElement.hasAttribute("data-numbered-hidden")) return;
-  var lineCount = textarea.parentElement.querySelector("ol"), previousCount = textarea.getAttribute("data-numbered-lines");
-  if (!previousCount) previousCount = 0;
-  var currentCount = ((textarea.value.match(/\n/g)||[]).length + 1), countDifference = currentCount - previousCount;
-  if (countDifference == 0) return;
-  if (countDifference > 0){
-    if (countDifference > 50){
-      var fragment = new DocumentFragment();
-      for (i = 0; i < countDifference; i++) fragment.appendChild(document.createElement("li"));
-      lineCount.appendChild(fragment);
-    } else {
-      for (i = 0; i < countDifference; i++) lineCount.appendChild(document.createElement("li"));
+  connectedCallback(){
+    this.addEventListener("mousedown",event => {
+      event.preventDefault();
+      if (document.activeElement != this.editor) this.editor.focus();
+    });
+    this.styles = document.createElement("link");
+    this.styles.rel = "stylesheet";
+    this.styles.href = "https://offroaders123.github.io/Numbered-Textarea-Plugin/styles.css";
+    this.container = document.createElement("div");
+    this.container.part = "container";
+    this.gutter = document.createElement("ol");
+    this.gutter.part = "gutter";
+    this.gutter.addEventListener("mousedown",event => {
+      event.preventDefault();
+      event.stopPropagation();
+      if (event.button != 0) return;
+      var index = Array.from(this.gutter.children).indexOf(event.target);
+      var line = indexi("\n",this.editor.value)[index - 1];
+      this.editor.setSelectionRange(line,line);
+      this.editor.blur();
+      this.editor.focus();
+      function indexi(char,str){
+        var list = [], i = -1;
+        while ((i = str.indexOf(char,i + 1)) >= 0) list.push(i + 1);
+        return list;
+      }
+    });
+    this.gutter.addEventListener("contextmenu",event => event.preventDefault());
+    this.gutter.addEventListener("scroll",() => this.refreshScrollPosition(),{ passive: true });
+    this.editor = document.createElement("textarea");
+    this.editor.part = "editor";
+    this.editor.placeholder = this.getAttribute("placeholder") || "";
+    this.editor.wrap = "off";
+    this.editor.spellcheck = false;
+    this.editor.autocomplete = "off";
+    this.editor.autocapitalize = "off";
+    this.editor.setAttribute("autocorrect","off");
+    this.editor.addEventListener("mousedown",event => event.stopPropagation());
+    this.editor.addEventListener("input",() => this.refreshLineNumbers());
+    this.editor.addEventListener("scroll",() => this.refreshScrollPosition(),{ passive: true });
+    new ResizeObserver(() => {
+      this.style.removeProperty("width");
+      this.style.height = `${this.offsetHeight - this.clientHeight + parseInt(this.editor.style.height,10)}px`;
+      this.editor.style.removeProperty("height");
+      this.refreshScrollPosition();
+    }).observe(this.editor);
+    this.shadowRoot.appendChild(this.styles);
+    this.shadowRoot.appendChild(this.container);
+    this.container.appendChild(this.gutter);
+    this.container.appendChild(this.editor);
+    this.disabled = this.matches("[disabled]");
+    this.editor.value = this.getAttribute("value") || "";
+    this.editor.setSelectionRange(0,0);
+    this.refreshLineNumbers();
+  }
+  refreshLineNumbers(){
+    var previousCount = this.editor.getAttribute("data-line-count") || 0,
+      count = (this.editor.value.match(/\n/g) || []).length + 1,
+      difference = count - previousCount;
+    if (difference == 0) return;
+    if (difference > 0){
+      var fragment = new DocumentFragment(), lineNumber = document.createElement("li");
+      lineNumber.part = "line-number";
+      for (var i = 0; i < difference; i++) fragment.appendChild(lineNumber.cloneNode());
+      this.gutter.appendChild(fragment);
     }
+    if (difference < 0) for (var i = 0; i < Math.abs(difference); i++) this.gutter.lastChild.remove();
+    this.editor.setAttribute("data-line-count",count);
+    this.refreshScrollPosition();
   }
-  if (countDifference < 0) for (i = 0; i < Math.abs(countDifference); i++) lineCount.lastChild.remove();
-  textarea.setAttribute("data-numbered-lines",currentCount);
-  textarea.num_refreshScrollPosition();
+  refreshScrollPosition(){
+    var { offsetHeight, clientHeight, scrollHeight, scrollTop } = this.editor;
+    var scrollbarHeight = offsetHeight - clientHeight, overscroll = (scrollTop < 0 || (clientHeight + scrollTop) > scrollHeight) ? (scrollTop < 0) ? scrollTop : (clientHeight + scrollTop) - scrollHeight : false;
+    if (scrollbarHeight > 0){
+      this.gutter.style.setProperty("--overflow-offset",`${scrollbarHeight}px`);
+    } else this.gutter.style.removeProperty("--overflow-offset");
+    if (overscroll == false){
+      this.gutter.style.removeProperty("--overscroll-top");
+      this.gutter.style.removeProperty("--overscroll-bottom");
+    } else (overscroll < 0) ? this.gutter.style.setProperty("--overscroll-top",`${Math.abs(overscroll)}px`) : this.gutter.style.setProperty("--overscroll-bottom",`${overscroll}px`);
+    if (this.gutter.scrollTop != this.editor.scrollTop) this.gutter.scrollTop = this.editor.scrollTop;
+  }
+  get value(){
+    return this.editor.value;
+  }
+  set value(text){
+    var active = document.activeElement;
+    this.editor.focus();
+    this.editor.select();
+    document.execCommand("insertText",null,text);
+    active.focus();
+    return text;
+  }
+  get disabled(){
+    return this.editor.disabled;
+  }
+  set disabled(state){
+    (state) ? this.setAttribute("disabled",true) : this.removeAttribute("disabled");
+    this.editor.disabled = state;
+  }
 }
-Element.prototype.num_refreshScrollPosition = function(){
-  var textarea = this, lineCount = textarea.parentElement.querySelector("ol"), scrollbarHeight = textarea.offsetHeight - textarea.clientHeight, paddingHeight = parseInt(window.getComputedStyle(lineCount).getPropertyValue("padding-top")), overflowOffset = parseInt(window.getComputedStyle(lineCount).getPropertyValue("--overflow-offset"));
-  if (scrollbarHeight > 0){
-    lineCount.style.setProperty("--overflow-offset",`${paddingHeight + scrollbarHeight}px`);
-  } else lineCount.style.setProperty("--overflow-offset",`${paddingHeight}px`);
-  lineCount.scrollTop = textarea.scrollTop;
-}
-Element.prototype.num_enableLineCount = function(){
-  var textarea = this;
-  textarea.parentElement.removeAttribute("data-numbered-hidden");
-  textarea.num_refreshLineCount();
-}
-Element.prototype.num_disableLineCount = function(){
-  var textarea = this;
-  textarea.parentElement.setAttribute("data-numbered-hidden",true);
-}
-Element.prototype.num_toggleLineCount = function(){
-  var textarea = this;
-  if (textarea.parentElement.hasAttribute("data-numbered-hidden")){
-    textarea.num_enableLineCount();
-  } else textarea.num_disableLineCount();
-}
-Element.prototype.num_changeValue = function(value){
-  var textarea = this;
-  if (textarea.tagName.toLowerCase() != "textarea") return console.error(`${textarea} is not a valid numbered textarea`,textarea);
-  if (!textarea.parentElement.hasAttribute("data-numbered-container")) return console.error(`${textarea} is not within a valid numbered container parent`,textarea);
-  textarea.value = value;
-  textarea.num_refreshLineCount();
-}
+window.customElements.define("num-text",NumText);
