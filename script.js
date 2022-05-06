460262ccb2c341c7a1e5428252ab701bb8b84536
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-05-06 04:45:37 -0700

    The Componentization!
    Awesome! So this was the part that I actually rewrote, earlier today (yesterday at this point now, hahaha).
    
    The main things:
    
    Changes:
    - No more Shadow DOM!
    - Component elements are now accessible through global page styles.
    - Rewrote a lot of the JS to use my more-recent JS practices, like const, and such. And also fixing my very-oversimplified lines that use ternary operators, glad I'm finally out of that phase. Now you can read them again! For a while I thought more compact was better. But in the end, it doesn't even maa.. (hehe, music inbound, that happens sometimes) ...I've changed my mind now that being able to read it better is more important :)
    
    Note: I haven't dont cross-browser testing yet, so there are probably a few oddities here and there. I want to start fresh and figure out what is still needed for certain fixes, and what it can do without. More changes soon!

--- script.js
+++ script.js
@@ -1,97 +1,114 @@
 class NumTextElement extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
-    this.attachShadow({ mode: "open" });
     this.defined = false;
   }
-  connectedCallback(){
+  connectedCallback() {
     if (this.defined || !this.isConnected) return;
     this.defined = true;
+    this.innerHTML = `<ol></ol><textarea wrap="off" spellcheck="false" autocomplete="off" autocapitalize="none" autocorrect="off"></textarea>`;
     this.addEventListener("mousedown",event => {
-      var target = event.composedPath()[0];
-      if (target == this.editor) return;
+      if (event.target === this.editor) return;
       event.preventDefault();
-      this.focus({ preventScroll: (!this.gutter.contains(target)) });
+      this.editor.focus({ preventScroll: !this.gutter.contains(event.target) });
     });
-    this.container = document.createElement("div");
-    this.container.part = "container";
-    this.gutter = document.createElement("ol");
-    this.gutter.part = "gutter";
     this.gutter.addEventListener("mousedown",event => {
-      var index = this.getLineIndexes()[Array.from(this.gutter.children).indexOf(event.target)];
-      this.editor.setSelectionRange(index,index);
+      const index = [...this.gutter.children].indexOf(event.target);
+      const lineIndex = this.lineIndices[index];
+      this.editor.setSelectionRange(lineIndex,lineIndex);
       this.blur();
     });
     this.gutter.addEventListener("dblclick",event => {
-      var indexes = this.getLineIndexes(), line = Array.from(this.gutter.children).indexOf(event.target);
-      this.editor.setSelectionRange(indexes[line],(line + 1 in indexes) ? indexes[line + 1] : this.editor.value.length);
+      const indices = this.lineIndices;
+      const line = [...this.gutter.children].indexOf(event.target);
+      const lineStartIndex = indices[line];
+      const lineEndIndex = (line + 1 in indices) ? indices[line + 1] : this.editor.value.length;
+      this.editor.setSelectionRange(lineStartIndex,lineEndIndex);
     });
-    this.gutter.addEventListener("scroll",() => this.refreshScrollPosition(),{ passive: true });
-    this.content = document.createElement("div");
-    this.content.part = "content";
-    this.editor = document.createElement("textarea");
-    this.editor.part = "editor";
-    this.editor.placeholder = this.getAttribute("placeholder") || "";
-    this.editor.wrap = "off";
-    this.editor.spellcheck = false;
-    this.editor.autocomplete = "off";
-    this.editor.autocapitalize = "none";
-    this.editor.setAttribute("autocorrect","off");
     this.editor.addEventListener("input",() => this.refreshLineNumbers());
     this.editor.addEventListener("scroll",() => this.refreshScrollPosition(),{ passive: true });
     new ResizeObserver(() => {
-      this.style.removeProperty("width");
-      this.style.height = `${this.offsetHeight - this.clientHeight + parseInt(this.editor.style.height,10)}px`;
-      this.editor.style.removeProperty("height");
-      this.refreshScrollPosition();
+      this.gutter.style.height = `${this.editor.offsetHeight}px`;
     }).observe(this.editor);
-    this.shadowRoot.appendChild(this.container);
-    this.container.appendChild(this.gutter);
-    this.container.appendChild(this.content);
-    this.content.appendChild(this.editor);
-    this.disabled = this.matches("[disabled]");
-    this.readonly = this.matches("[readonly]");
-    this.editor.value = this.getAttribute("value") || "";
     this.editor.setSelectionRange(0,0);
     this.refreshLineNumbers();
   }
-  refreshLineNumbers(){
-    var previousCount = this.getAttribute("line-count") || 0, count = (this.editor.value.match(/\n/g) || []).length + 1, difference = count - previousCount;
-    if (difference == 0) return;
+  get editor() {
+    return this.querySelector("textarea");
+  }
+  get gutter() {
+    return this.querySelector("ol");
+  }
+  get lineCount() {
+    return (this.editor.value.match(/\n/g) || []).length + 1;
+  }
+  refreshLineNumbers() {
+    const previous = this.getAttribute("line-count") || 0;
+    const next = this.lineCount;
+    const difference = next - previous;
+    if (difference === 0) return;
     if (difference > 0){
-      var fragment = new DocumentFragment(), lineNumber = document.createElement("li");
-      lineNumber.part = "line-number";
-      for (var i = 0; i < difference; i++) fragment.appendChild(lineNumber.cloneNode());
-      this.gutter.appendChild(fragment);
+      const fragment = new DocumentFragment();
+      const line = document.createElement("li");
+      for (let i = 0; i < difference; i++) fragment.append(line.cloneNode());
+      this.gutter.append(fragment);
     }
-    if (difference < 0) for (var i = 0; i < Math.abs(difference); i++) this.gutter.lastChild.remove();
-    this.setAttribute("line-count",count);
-    this.refreshScrollPosition();
+    if (difference < 0){
+      for (let i = 0; i < Math.abs(difference); i++){
+        this.gutter.lastChild.remove();
+      }
+    }
+    this.setAttribute("line-count",next);
   }
-  getCharacterIndexes(character){
-    var list = [], i = -1;
-    while ((i = this.editor.value.indexOf(character,i + 1)) >= 0) list.push(i + 1);
-    return list;
+  getStringIndices(string) {
+    const matches = [...this.editor.value.matchAll(new RegExp(string,"g"))];
+    return matches.map(match => match.index);
   }
-  getLineIndexes(){
-    var indexes = this.getCharacterIndexes("\n");
-    indexes.unshift(0);
-    return indexes;
+  get lineIndices() {
+    const indices = this.getStringIndices("\n");
+    for (const index in indices) indices[index]++;
+    indices.unshift(0);
+    return indices;
   }
-  refreshScrollPosition(){
-    var { offsetWidth, offsetHeight, clientWidth, clientHeight, scrollWidth, scrollHeight, scrollLeft, scrollTop } = this.editor;
-    var scrollbarWidth = offsetWidth - clientWidth, scrollbarHeight = offsetHeight - clientHeight, overscrollX = (scrollLeft < 0 || (clientWidth + scrollLeft) > scrollWidth) ? (scrollLeft < 0) ? scrollLeft : (clientWidth + scrollLeft) - scrollWidth : false, overscrollY = (scrollTop < 0 || (clientHeight + scrollTop) > scrollHeight) ? (scrollTop < 0) ? scrollTop : (clientHeight + scrollTop) - scrollHeight : false;
-    (scrollbarWidth > 0) ? this.container.style.setProperty("--overflow-offset-x",`${scrollbarWidth}px`) : this.container.style.removeProperty("--overflow-offset-x");
-    (scrollbarHeight > 0) ? this.container.style.setProperty("--overflow-offset-y",`${scrollbarHeight}px`) : this.container.style.removeProperty("--overflow-offset-y");
-    if (overscrollX == false){
-      this.container.style.removeProperty("--overscroll-left");
-      this.container.style.removeProperty("--overscroll-right");
-    } else (overscrollX < 0) ? this.container.style.setProperty("--overscroll-left",`${Math.abs(overscrollX)}px`) : this.container.style.setProperty("--overscroll-right",`${overscrollX}px`);
-    if (overscrollY == false){
-      this.container.style.removeProperty("--overscroll-top");
-      this.container.style.removeProperty("--overscroll-bottom");
-    } else (overscrollY < 0) ? this.container.style.setProperty("--overscroll-top",`${Math.abs(overscrollY)}px`) : this.container.style.setProperty("--overscroll-bottom",`${overscrollY}px`);
-    if (this.gutter.scrollTop != scrollTop) this.gutter.scrollTop = scrollTop;
+  refreshScrollPosition() {
+    const { offsetWidth, offsetHeight, clientWidth, clientHeight, scrollWidth, scrollHeight, scrollLeft, scrollTop } = this.editor;
+    const scrollRight = clientWidth + scrollLeft;
+    const scrollBottom = clientHeight + scrollTop;
+    const scrollbarWidth = offsetWidth - clientWidth;
+    const scrollbarHeight = offsetHeight - clientHeight;
+    const overscrollX = (scrollLeft < 0 || scrollRight > scrollWidth) ? scrollLeft < 0 ? scrollLeft : scrollRight - scrollWidth : false;
+    const overscrollY = (scrollTop < 0 || scrollBottom > scrollHeight) ? scrollTop < 0 ? scrollTop : scrollBottom - scrollHeight : false;
+
+    if (scrollbarWidth > 0){
+      this.gutter.style.setProperty("--overflow-offset-x",`${scrollbarWidth}px`);
+    } else {
+      this.gutter.style.removeProperty("--overflow-offset-x");
+    }
+    if (scrollbarHeight > 0){
+      this.gutter.style.setProperty("--overflow-offset-y",`${scrollbarHeight}px`);
+    } else {
+      this.gutter.style.removeProperty("--overflow-offset-y");
+    }
+
+    if (overscrollX === false){
+      this.gutter.style.removeProperty("--overscroll-left");
+      this.gutter.style.removeProperty("--overscroll-right");
+    } else if (overscrollX < 0){
+      this.gutter.style.setProperty("--overscroll-left",`${Math.abs(overscrollX)}px`);
+    } else if (overscrollX > 0){
+      this.gutter.style.setProperty("--overscroll-right",`${overscrollX}px`);
+    }
+    if (overscrollY === false){
+      this.gutter.style.removeProperty("--overscroll-top");
+      this.gutter.style.removeProperty("--overscroll-bottom");
+    } else if (overscrollY < 0){
+      this.gutter.style.setProperty("--overscroll-top",`${Math.abs(overscrollY)}px`);
+    } else if (overscrollY > 0){
+      this.gutter.style.setProperty("--overscroll-bottom",`${overscrollY}px`);
+    }
+
+    if (this.gutter.scrollTop !== scrollTop) this.gutter.scrollTop = scrollTop;
   }
 }
+
 window.customElements.define("num-text",NumTextElement);
