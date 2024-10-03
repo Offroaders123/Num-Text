5ee0d005e3844c98a64936a0dfa506cfdc8bd777
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-10-02 23:03:23 -0400

    Polyfills Removal
    Now that I'm not using CSS Module Imports, and that Constructable Stylesheets are supported in all browsers, no need for polyfills anymore! (In theory of course)

--- test/polyfill.ts
+++
@@ -1,6 +0,0 @@
-window.esmsInitOptions = {
-  polyfillEnable: ["css-modules"]
-};
-
-await import("https://cdn.jsdelivr.net/npm/construct-style-sheets-polyfill");
-await import("https://cdn.jsdelivr.net/npm/es-module-shims");
